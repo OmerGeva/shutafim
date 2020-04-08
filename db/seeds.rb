405357bee4a987b53cc3e48dec1064ce893a87d7
Alisa Silina <alisasilina@gmail.com>
2020-04-08 19:10:29 +0300

    users seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+omer = User.create(
+  email: 'omer@geva.com',
+  password: '123456',
+  first_name: 'Omer',
+  last_name: 'Geva',
+  username: 'omergeva'
+  )
+omer.remote_photo_url = 'https://avatars2.githubusercontent.com/u/54701359?v=4'
+omer.save
+
+brad = User.create(
+  email: 'brad@korman.com',
+  password: '123456',
+  first_name: 'Brad',
+  last_name: 'Korman',
+  username: 'bradkorman'
+  )
+brad.remote_photo_url = 'https://avatars2.githubusercontent.com/u/54051432?v=4'
+brad.save
+
+alisa = User.create(
+  email: 'alisa@silina.com',
+  password: '123456',
+  first_name: 'Alisa',
+  last_name: 'Silina',
+  username: 'alisasilina'
+  )
+alisa.remote_photo_url = 'https://avatars0.githubusercontent.com/u/54353429?v=4'
+alisa.save
