ec070b5550f8ac9d09cd3750db771de1f7177aeb
Omer Geva <ogeva97@gmail.com>
2020-04-08 10:33:28 +0300

    traits available to add on create profile page

--- db/migrate/20200407021032_create_traits.rb
+++ db/migrate/20200407021032_create_traits.rb
@@ -1,10 +1,10 @@
 class CreateTraits < ActiveRecord::Migration[5.2]
   def change
     create_table :traits do |t|
-      t.boolean :smokes
-      t.boolean :has_cat
-      t.boolean :has_dog
-      t.boolean :veg
+      t.boolean :smokes, default: false
+      t.boolean :has_cat, default: false
+      t.boolean :has_dog, default: false
+      t.boolean :veg, default: false
     end
   end
 end
