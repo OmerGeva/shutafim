10910d4c3895bb0210fc8097b62e91364f7954c5
Alisa Silina <alisasilina@gmail.com>
2020-04-08 23:48:52 +0300

    created part of fake seeds

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'redis', '~> 4.0'
-
+gem 'faker'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,60 @@
+require 'faker'
+
 # Users
 omer = User.create(
   email: 'omer@geva.com',
   password: '123456',
   first_name: 'Omer',
   last_name: 'Geva',
-  username: 'omergeva'
+  phone_number: rand(10 ** 10),
+  dob: Faker::Date.birthday(min_age: 18, max_age: 65),
   )
-omer.remote_photo_url = 'https://avatars2.githubusercontent.com/u/54701359?v=4'
-omer.save
 
 brad = User.create(
   email: 'brad@korman.com',
   password: '123456',
   first_name: 'Brad',
   last_name: 'Korman',
-  username: 'bradkorman'
-  )
-brad.remote_photo_url = 'https://avatars2.githubusercontent.com/u/54051432?v=4'
-brad.save
+  phone_number: rand(10 ** 10),
+  dob: Faker::Date.birthday(min_age: 18, max_age: 65),
+)
 
 alisa = User.create(
   email: 'alisa@silina.com',
   password: '123456',
   first_name: 'Alisa',
   last_name: 'Silina',
-  username: 'alisasilina'
-  )
-alisa.remote_photo_url = 'https://avatars0.githubusercontent.com/u/54353429?v=4'
-alisa.save
+  phone_number: rand(10 ** 10),
+  dob: Faker::Date.birthday(min_age: 18, max_age: 65),
+)
+
+# Amenities
+apt1_amenity = Amenity.create(
+  laundry_washer: true,
+  air_conditioner: true
+)
+
+# Cities
+tel_aviv = City.create(
+  name: 'Tel Aviv'
+)
+
+# Apartments
+apartment1 = Apartment.create(
+  bedrooms: 3,
+  bathrooms: 2,
+  total_rooms: 6,
+  description: Faker::Lorem.sentence(word_count: 8),
+  street_address: Faker::Address.street_address,
+  city_id: tel_aviv,
+  amenity_id: apt1_amenity,
+  photo: 'https://source.unsplash.com/1600x900/?apartment,room'
+)
+
+# Listings
+listing1 = Listing.create(
+  user: brad,
+  price: rand(1000..3000),
+  duration: 1,
+  apartment_id: apartment1
+)
